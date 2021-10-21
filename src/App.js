f49f60c5e216fa395b745aec2c0719db46ff1165
Ashish Mishra <you@example.com>
2021-10-21 11:41:09 +0530

    my second edit commit

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ export default class App extends Component
        <Router>
         <Navabar/> 
         <Switch>
-          <Route exact path="/newsmasala/"><News key="general" pageSize={5} category="general"/></Route> 
-          <Route exact path="/newsmasala/business"><News key="business" pageSize={5} category="business"/></Route> 
-          <Route exact path="/newsmasala/entertainment"><News key="entertainment" pageSize={5} category="entertainment"/></Route> 
-          <Route exact path="/newsmasala/general"><News key="general" pageSize={5} category="general"/></Route> 
-          <Route exact path="/newsmasala/health"><News key="health" pageSize={5} category="health"/></Route> 
-          <Route exact path="/newsmasala/science"><News key="science" pageSize={5} category="science"/></Route> 
-          <Route exact path="/newsmasala/sports"><News key="sports" pageSize={5} category="sports"/></Route> 
-          <Route exact path="/newsmasala/technology"><News key="technology" pageSize={5} category="technology"/></Route> 
+          <Route exact path="/"><News key="general" pageSize={5} category="general"/></Route> 
+          <Route exact path="/business"><News key="business" pageSize={5} category="business"/></Route> 
+          <Route exact path="/entertainment"><News key="entertainment" pageSize={5} category="entertainment"/></Route> 
+          <Route exact path="/general"><News key="general" pageSize={5} category="general"/></Route> 
+          <Route exact path="/health"><News key="health" pageSize={5} category="health"/></Route> 
+          <Route exact path="/science"><News key="science" pageSize={5} category="science"/></Route> 
+          <Route exact path="/sports"><News key="sports" pageSize={5} category="sports"/></Route> 
+          <Route exact path="/technology"><News key="technology" pageSize={5} category="technology"/></Route> 
         </Switch>
         </Router>
       </>
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -9,7 +9,7 @@ export default class Newsitem extends Component {
             <>
 
                 <div className="card" style={{ width: "" }}>
-                    <span className="position-absolute top-1 start-70 translate-middle badge rounded-pill bg-danger ">
+                    <span className="position-absolute top-1 start-100 translate-middle badge rounded-pill bg-danger " style={{margin:' -10px -37px'}}>
                         {source}
                         <span className="visually-hidden">unread messages</span>
                     </span>
@@ -18,7 +18,7 @@ export default class Newsitem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}......</h5>
                         <p className="card-text">{description}......</p>
-                        <p className="card-text"  style={{color:"#05074c"}}><small className="text-bold">By {author} on {new Date(date).toGMTString()}</small></p>
+                        <p className="card-text"  style={{color:"#05074c"}}><small className="text-bold">By <span className="text-bold" style={{color:'green'}}>{source}</span> on {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={url} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
